452287ab8f23352ba8574802126d85e76e59d81e
riteshpal42005-dotcom <riteshpal42005@gmail.com>
2026-05-08 20:17:19 +0530

    Added Python PostgreSQL CSV upload script
    Created a Python script to upload CSV data into PostgreSQL using Pandas and SQLAlchemy

---
+++ PYTHON SQL PROJECTY/upload.py
@@ -0,0 +1,25 @@
+import pandas as pd
+from sqlalchemy import create_engine
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+if not DATABASE_URL:
+    raise ValueError("DATABASE_URL environment variable not set.")
+
+engine = create_engine(DATABASE_URL)
+
+df = pd.read_excel("E-Commerce Orders.csv")
+
+df.to_sql(
+    "customers",
+    engine,
+    if_exists="replace",
+    index=False
+)
+
+print("Uploaded Successfully")
