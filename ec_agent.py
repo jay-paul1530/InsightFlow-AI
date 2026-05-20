1ec25de122b6da4eb7dfd2f8d75852d087781daa
dipeshpal <dipeshpal17@gmail.com>
2026-05-20 23:15:13 +0530

    Update ec_agent.py

--- ec_agent.py
+++ ec_agent.py
@@ -3,6 +3,8 @@ from services.ecommerce_agent.run_ecommerce_agent import run_ecommerce_agent
 
 
 if __name__ == "__main__":
-    user_input = "Give me top 5 orders for unit price above 250, and create graph between unitprice and totalorder"
-    response = asyncio.run(run_ecommerce_agent(user_input))
-    print("AI Response:", response)
+    while True:
+        # user_input = "Give me top 5 orders for unit price above 250, and create graph between unitprice and totalorder"
+        user_input = input("User: ")
+        response = asyncio.run(run_ecommerce_agent(user_input))
+        print("AI Response:", response)
