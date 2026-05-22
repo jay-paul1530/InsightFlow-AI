2de0ea346d42bbf1cf60cee56b9b3ed2f261e328
Danishkhan27 <73233875+Danishkhan27@users.noreply.github.com>
2026-05-23 00:49:45 +0530

    Task04
    Completed

--- env.py
+++ env.py
@@ -5,7 +5,7 @@ load_dotenv()
 
 DATASET_DIR = os.getenv("DATASET_DIR")
 FILE_NAME = os.getenv("FILE_NAME")
-VALUE_OF_INR = float(os.getenv("VALUE_OF_INR"))
+VALUE_OF_INR = float(os.getenv("INR_RATE", 1.0))
 TABLE_NAME = os.getenv("TABLE_NAME")
 
 OPENROUTER_API_KEY = os.getenv("OPENROUTER_API_KEY")
@@ -14,11 +14,11 @@ OPENROUTER_URL = os.getenv("OPENROUTER_URL")
 
 MCP_SANDBOX_URL = os.getenv("MCP_SANDBOX_URL")
 
-DB_HOSTNAME = os.getenv("DB_HOSTNAME")
-DB_PORT = os.getenv("DB_PORT")
-DB_NAME = os.getenv("DB_NAME")
-DB_USER = os.getenv("DB_USER")
-DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOSTNAME = os.getenv("Hostname")
+DB_PORT = os.getenv("DB_Port")
+DB_NAME = os.getenv("DB_Name")
+DB_USER = os.getenv("DB_User")
+DB_PASSWORD = os.getenv("DB_Password")
 
 DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOSTNAME}:{DB_PORT}/{DB_NAME}" if (os.getenv("DATABASE_URL") == "" or os.getenv("DATABASE_URL") is None) else os.getenv("DATABASE_URL")
 
