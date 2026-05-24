6d3e9e82a48b76e833a67ab74b0c1b51363da2d3
Danishkhan27 <73233875+Danishkhan27@users.noreply.github.com>
2026-05-24 14:32:38 +0530

    task03

--- ec_agent.py
+++ ec_agent.py
@@ -2,7 +2,30 @@ import asyncio
 from services.ecommerce_agent.run_ecommerce_agent import run_ecommerce_agent
 
 
+messages = []
+
+def chatbot():
+    while True:
+        user_input = input("User: ")
+        
+        if len(messages) > 5:
+            messages.pop(0)
+            
+        context_str = ""
+        if messages:
+            context_str = "Previous Conversation History:\n"
+            for msg in messages:
+                context_str += f"User: {msg['User']}\nAI: {msg['AI']}\n"
+            context_str += "\nCurrent Request: "
+            
+        full_input = context_str + user_input
+        
+        response = asyncio.run(run_ecommerce_agent(user_input=full_input))
+        messages.append({"User": user_input, "AI": response})
+        print("AI Response:", response)
+
+
+
 if __name__ == "__main__":
-    user_input = "Give me top 5 orders for unit price above 250, and create graph between unitprice and totalorder"
-    response = asyncio.run(run_ecommerce_agent(user_input))
-    print("AI Response:", response)
+    chatbot()
+
