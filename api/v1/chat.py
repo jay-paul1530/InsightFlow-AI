99e9e97047c1f736c1042a6addbd2c91555cbeee
dipeshpal <dipeshpal17@gmail.com>
2026-05-27 23:11:03 +0530

    Update chat.py

--- api/v1/chat.py
+++ api/v1/chat.py
@@ -3,12 +3,12 @@
 # # Chat Post API
 # """
 # If new message then create new session:
-#     AI Chat Generation
+#     AI Chat Generation  *****
 #     Save new session in session db
 #     Save chat in chat db
 # Else:
 #     append new message in last chat session
-#     AI Chat Generation
+#     AI Chat Generation *****
 #     Save chat in chat db
 # """
 
