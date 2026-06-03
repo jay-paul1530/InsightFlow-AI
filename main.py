aca925afcc2f0d6379b1d1f933acbad41cc80ff5
jay-paul1530 <jaypaul1515@gmail.com>
2026-06-03 19:10:29 +0530

    Created APIs for chat_session
    Created two APIs, one to list all the sessions, and one to list all the messages in the respected session.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from api.v1.healthcheck import router as healthcheck_router
 from api.v1.dummy import router as dummy_router
 from api.v1.chat import router as chat_router
+from api.v1.chat_session import router as chat_session_router
 from core.database import Base, engine
 import models.chat
 import models.chat_session
@@ -12,7 +13,11 @@ app = FastAPI()
 app.include_router(healthcheck_router, prefix="/api/v1", tags=["Healthcheck"])
 app.include_router(dummy_router, prefix="/api/v1", tags=["Dummy"])
 app.include_router(chat_router, prefix="/api/v1", tags=["Chat"])
-
+app.include_router(
+    chat_session_router,
+    prefix="/api/v1",
+    tags=["Chat Sessions"]
+)
 
 if __name__ == "__main__":
     import uvicorn
